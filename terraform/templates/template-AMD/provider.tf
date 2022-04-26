84144e6140c1fcd981571f38df379351c835db77
alilakhani786 <73431376+alilakhani786@users.noreply.github.com>
2022-04-26 11:21:17 +0800

    Initial project creation for Adaptive Media Delivery (AMD)

---
+++ terraform/templates/template-AMD/provider.tf
@@ -0,0 +1,13 @@
+/*
+  Author: Noorali (Ali) Lakhani @ nlakhani@akamai.com
+  Creation date: 03/03/2022
+  Purpose: Automate Onboarding Akamai Delivery Configuration 
+  Repo: https://github.com/alilakhani786/akamai/tree/main/terraform
+
+  main.tf is the entry point to terraform project
+*/
+
+provider "akamai" {
+  edgerc         = "~/.edgerc"
+  config_section = ""
+}
