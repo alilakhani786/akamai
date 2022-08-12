c7a28a989d744cea2573bb59acff754eb356b08a
Syah21 <94879897+SyahirahAdni@users.noreply.github.com>
2022-08-12 15:47:48 +0800

    Create main.tf

---
+++ terraform/templates/template-edgeDNS/add zone-record/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    akamai = {
+      source = "akamai/akamai"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+
+provider "akamai" {
+  edgerc         = "~/.edgerc"
+  config_section = "default"
+}
+
+
+
+locals {}
+
+//data "akamai_group" "group" {}
+
+data "akamai_group" "group" {
+  group_name  = var.group_name
+  contract_id = var.contract_id
+}
+
+//data "akamai_contract" "contract" {}
+
+data "akamai_contract" "contract" {
+  group_name = var.group_name
+}
+
+resource "akamai_edge_hostname" "edgehostname" {
+  product_id    = var.product_id
+  contract_id   = data.akamai_contract.contract.id
+  group_id      = data.akamai_group.group.id
+  edge_hostname = var.edgeHostname
+  ip_behavior   = var.ip_behavior
+
+}
+
+resource "akamai_dns_zone" "test_primary_zone" {
+  contract       = data.akamai_contract.contract.id
+  group          = data.akamai_group.group.id
+  zone           = var.domain
+  type           = "PRIMARY"
+  comment        = "This is a test  primary zone"
+  sign_and_serve = true
+  sign_and_serve_algorithm = "ECDSA_P256_SHA256"
+}
+
+data "akamai_authorities_set" "ns" {
+  contract = data.akamai_contract.contract.id
+}
+
+resource "akamai_dns_record" "CNAME_record" {
+  zone       = var.domain
+  name       = var.hostname  
+  recordtype = "CNAME"
+  active     = true
+  ttl        = 300
+  target     = ["${var.edgeHostname}"]   // "wwww" can be replaced with preferred choice
+}
+
+
