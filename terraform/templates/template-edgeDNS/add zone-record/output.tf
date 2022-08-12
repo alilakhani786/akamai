15e6f2b69ac51804c60d3a1e4f24fc0c5ca9f340
Syah21 <94879897+SyahirahAdni@users.noreply.github.com>
2022-08-12 15:53:31 +0800

    Add files via upload

---
+++ terraform/templates/template-edgeDNS/add zone-record/output.tf
@@ -0,0 +1,36 @@
+output "group_name" {
+  value     = data.akamai_group.group.group_name
+  sensitive = false
+}
+
+output "contract_id" {
+  value     = data.akamai_group.group.contract_id
+  sensitive = false
+}
+
+output "edgeHostname" {
+  value     = akamai_edge_hostname.edgehostname.edge_hostname
+  sensitive = false
+}
+
+output "ip_behavior" {
+  value     = akamai_edge_hostname.edgehostname.ip_behavior
+  sensitive = false
+}
+
+
+
+output "hostname" {
+  value     = akamai_dns_record.CNAME_record.name
+  sensitive = false
+}
+
+
+
+
+
+
+output "domain" {
+  value     = akamai_dns_zone.test_primary_zone.zone
+  sensitive = false
+}
---
+++ terraform/templates/template-edgeDNS/add zone-record/variable.tf
@@ -0,0 +1,8 @@
+variable "group_name" {}
+variable "contract_id" {}
+variable "edgeHostname" {}
+variable "hostname" {}
+variable "contact" {}
+variable "domain" {}
+variable "ip_behavior" {}
+variable "product_id" {}
