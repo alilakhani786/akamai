1c1b0b12870dece4b85a2b2e56798d331969fbfb
Ali Lakhani <nlakhani@sin-mpuet.localdomain>
2024-07-01 14:14:16 +0800

    initial working project

--- akamai-cda-client/server.js
+++ akamai-cda-client/server.js
@@ -1,1 +1,23 @@
-/* Server.js entrypoint */
+const express = require("express");
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+
+/*
+app.get('/', (req, res) => {
+    res.send("Hello from CDA NodeJS Server");
+});
+*/
+
+app.get('/', (req, res) => {
+    res.render('home', {
+      title: 'Akamai CDA Portal',
+    });
+  });
+
+const server = app.listen(process.env.PORT || 3001, () => {
+    console.log(`CDA server started on port: ${server.address().port}`);
+});
