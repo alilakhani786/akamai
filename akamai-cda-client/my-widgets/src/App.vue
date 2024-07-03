4e71dd9662e0979f35d9fb2ba8d8d0aa58aeaa02
Lynette Lee <lleesiyi@sin-wp3tu.singapore.corp.akamai.com>
2024-07-03 18:04:36 +0800

    building Cards

---
+++ akamai-cda-client/my-widgets/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Cards />
+  </div>
+</template>
+
+<script>
+import Cards from './components/Cards.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Cards,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  color: #2c3e50;
+  margin-top: 60px;
+  height: 100vh
+}
+</style>
---
+++ akamai-cda-client/my-widgets/src/components/Cards.vue
@@ -0,0 +1,94 @@
+<template>
+    <CRow :md="{ cols: 3 }">
+        <CCol>
+            <CCard>
+                <CCardImage orientation="top" src="https://images.app.goo.gl/XUFLpHG8fHQP7Bow5" />
+                <CCardBody>
+                    <CCardTitle>Card title</CCardTitle>
+                    <CCardText>This is a wider card with supporting text below as a natural lead-in to additional
+                        content.
+                        This content is a little bit longer.</CCardText>
+                    <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
+                </CCardBody>
+            </CCard>
+        </CCol>
+        <CCol>
+            <CCard>
+                <CCardImage orientation="top" src="https://images.app.goo.gl/XUFLpHG8fHQP7Bow5" />
+                <CCardBody>
+                    <CCardTitle>Card title</CCardTitle>
+                    <CCardText>This is a wider card with supporting text below as a natural lead-in to additional
+                        content.
+                        This content is a little bit longer.</CCardText>
+                    <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
+                </CCardBody>
+            </CCard>
+        </CCol>
+        <CCol>
+            <CCard>
+                <CCardImage orientation="top" src="https://images.app.goo.gl/XUFLpHG8fHQP7Bow5" />
+                <CCardBody>
+                    <CCardTitle>Card title</CCardTitle>
+                    <CCardText>This is a wider card with supporting text below as a natural lead-in to additional
+                        content.
+                        This content is a little bit longer.</CCardText>
+                    <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
+                </CCardBody>
+            </CCard>
+        </CCol>
+        <CCol>
+            <CCard>
+            <CCardImage orientation="top" src="https://images.app.goo.gl/XUFLpHG8fHQP7Bow5" />
+            <CCardBody>
+                <CCardTitle>Card title</CCardTitle>
+                <CCardText>This is a wider card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.</CCardText>
+                <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
+            </CCardBody>
+        </CCard>
+        </CCol>
+        <CCol>
+            <CCard>
+            <CCardImage orientation="top" src="https://images.app.goo.gl/XUFLpHG8fHQP7Bow5" />
+            <CCardBody>
+                <CCardTitle>Card title</CCardTitle>
+                <CCardText>This is a wider card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.</CCardText>
+                <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
+            </CCardBody>
+        </CCard>
+        </CCol>
+        <CCol>
+            <CCard>
+            <CCardImage orientation="top" src="https://images.app.goo.gl/XUFLpHG8fHQP7Bow5" />
+            <CCardBody>
+                <CCardTitle>Card title</CCardTitle>
+                <CCardText>This is a wider card with supporting text below as a natural lead-in to additional content.
+                    This content is a little bit longer.</CCardText>
+                <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
+            </CCardBody>
+        </CCard>
+        </CCol>
+    </CRow>
+</template>
+
+
+
+
+<script>
+import { CCard, CCardGroup, CCardText, CCardBody, CCardTitle, CRow, CCol } from '@coreui/vue';
+import '@coreui/coreui/dist/css/coreui.min.css'
+
+export default {
+    name: 'cards',
+    components: {
+        CCard,
+        CCardGroup,
+        CCardText,
+        CCardBody,
+        CCardTitle,
+        CCol,
+        CRow
+    },
+};
+</script>
