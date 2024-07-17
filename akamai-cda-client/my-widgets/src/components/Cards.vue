fc0684238d356fd69b2a0e17f686cb5d23901476
Lynette <lynetteleesy@gmail.com>
2024-07-17 18:11:32 +0800

    fetched api to client cards

--- akamai-cda-client/my-widgets/src/components/Cards.vue
+++ akamai-cda-client/my-widgets/src/components/Cards.vue
@@ -1,12 +1,12 @@
 <template>
-    <CRow :md="{ cols: 3 }">
+    <CRow :md="{ cols: 2 }">
         <CCol>
             <CCard text-color="info" class="mb-3 border-top-3"  :class="'border-' + 'info'">
                 <!-- <CCardImage orientation="top" src="akamaiImage" /> -->
                 <img src='@/assets/Akamai.png'/>
                 <CCardBody>
-                    <CCardTitle>Card title</CCardTitle>
-                    <CCardText>The company offers cloud security, mobile and web performance, carrier, and media delivery solutions. </CCardText>
+                    <CCardTitle>CP Code Reporting Group</CCardTitle>
+                    <CCardText v-for="item in info" :key="item.id">{{ item.id }} - {{ item.name }}</CCardText>
                     <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
                 </CCardBody>
             </CCard>
@@ -17,22 +17,22 @@
                 <img src='@/assets/Akamai.png'/>
                 <CCardBody>
                     <CCardTitle>Card title</CCardTitle>
-                    <CCardText>The company offers cloud security, mobile and web performance, carrier, and media delivery solutions.</CCardText>
+                    <CCardText>The company of cloud security, mobile and web performance, carrier, and media delivery solutions.</CCardText>
                     <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
                 </CCardBody>
             </CCard>
         </CCol>
-        <CCol>
+        <!-- <CCol>
             <CCard color="info">
-                <!-- <CCardImage orientation="top" src="../src/assets/Akamai.png" /> -->
-                <img src='@/assets/Akamai.png'/>
+                <CCardImage orientation="top" src="../src/assets/Akamai.png" /> -->
+                <!-- <img src='@/assets/Akamai.png'/>
                 <CCardBody>
                     <CCardTitle>Card title</CCardTitle>
                     <CCardText>The company offers cloud security, mobile and web performance, carrier, and media delivery solutions.</CCardText>
                     <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
                 </CCardBody>
             </CCard>
-        </CCol>
+        </CCol> -->
     </CRow>
 </template>
 
@@ -42,7 +42,7 @@
 <script>
 import { CCard, CCardGroup, CCardText, CCardBody, CCardTitle, CRow, CCol, CCardImage } from '@coreui/vue';
 import '@coreui/coreui/dist/css/coreui.min.css';
-import akamaiImage from "@/assets/Akamai.png"
+import axios from 'axios';
 
 export default {
     name: 'cards',
@@ -56,6 +56,18 @@ export default {
         CRow,
         CCardImage,
     },
-
+    data() {
+        return {
+            info: []
+        }
+    },
+    mounted () {
+        axios
+        .get('http://localhost:3000/akamai')
+        .then(response => (this.info = response.data))
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            });
+  }
 };
 </script>
